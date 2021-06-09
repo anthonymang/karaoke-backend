22d9aa608605eb5aa8d683f04f5fa50c4cc87686
davemolk <davemolk@gmail.com>
2021-06-09 10:00:35 -0600

    revised User and Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const commentSchema = new Schema({
-  user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  video: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
   content: String,
   likes: Number,
   timestamps: true,
